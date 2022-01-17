249f24e264c6acbf0106f9e9772ec87e8771393c
Cris Vallejo <86570881+crisvarai@users.noreply.github.com>
2022-01-17 00:44:31 -0600

    Update Jacobiano_Benchmark_One_Pass.py

--- Jacobiano_Benchmark_One_Pass.py
+++ Jacobiano_Benchmark_One_Pass.py
@@ -1,3 +1,10 @@
+# ---------------------- Información del Script --------------------------
+
+# PROGRAMA: Que efectúa el benchmark (2) para el Jacobiano Analítico
+# OBJETIVO: Anilizar el tiempo de cálculo que tomará calcular la matriz Jacobiana
+# FECHA:    15 de Julio 2021
+# DISEÑO:   Ing. Cristian Vallejo
+
 import numpy as np
 import timeit                               # For timing the durations
 
